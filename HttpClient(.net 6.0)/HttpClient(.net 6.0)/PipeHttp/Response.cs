5f09c5ddac7785db2372760ef1648e0aa206ee4f
AplyQ8 <kirill.smith.98@inbox.ru>
2022-12-09 17:09:12 +0300

    Relocated to new SDK version
    (.NET 6.0 LTS)

---
+++ HttpClient(.net 6.0)/HttpClient(.net 6.0)/PipeHttp/Response.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClient_.net_6._0_.PipeHttp
+{
+    public class Response
+    {
+        public static async Task ResponseHttp(TcpClient tcpClient)
+        {
+            var stream = tcpClient.GetStream();
+            var responseData = new byte[512];
+
+            var response = new StringBuilder();
+            int bytes;
+            do
+            {
+                bytes = await stream.ReadAsync(responseData);
+                response.Append(Encoding.UTF8.GetString(responseData, 0, bytes));
+            }
+            while (tcpClient.Available > 0);
+
+            Console.WriteLine(response);
+        }
+    }
+}
---
+++ HttpClient(.net 6.0)/HttpClient(.net 6.0)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using HttpClient_.net_6._0_.PipeHttp;
+
+namespace HttpClient_.net_6._0_
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using TcpClient tcpClient = new TcpClient();
+
+            await Connect.CreateConn(tcpClient);
+
+            await Request.RequestHttp(tcpClient);
+
+            await Response.ResponseHttp(tcpClient);
+
+        }
+    }
+
+}
